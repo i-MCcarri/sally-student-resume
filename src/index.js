036d8906ab8f9fb3d44acd6cd3e0138b1ae3e13c
Adyceum A. Magna Ccarri <61805757+i-MCcarri@users.noreply.github.com>
2020-08-25 07:25:58 -0700

    personal copy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './composition/App';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* Note: 
+document.getElementById('root') achieves the same result as 
+document.querySelector('#root'). It doesn't matter which one 
+you use. The querySelector method works with any CSS selector 
+and getElementById only works with ids.  */
